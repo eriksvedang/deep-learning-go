40bd9bba12746e4cb31a26889ae6a9b0d7997c92
Erik Svedäng <erik.svedang@gmail.com>
2026-02-25 18:16:46 +0100

    Board class.

--- dlgo/__init__.py
+++
@@ -1,7 +0,0 @@
-import gotypes
-
-player = gotypes.Player.black
-print(player.other)
-
-point = gotypes.Point(10, 10)
-print(point.neighbors())
--- dlgo/main.py
+++ dlgo/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from dlgo!")
+import gotypes
+import goboard_slow
 
+def main():
+    player = gotypes.Player.black
+    print(player.other)
+    point = gotypes.Point(10, 10)
+    print(point.neighbors())
+    board = goboard_slow.Board(19, 19)
+    board.place_stone(player, point)
+    board.place_stone(player, gotypes.Point(2, 3))
+    print(board._grid)
 
 if __name__ == "__main__":
     main()
