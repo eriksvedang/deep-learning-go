3076e39f463bbf0f9134036c266855d8cee2411e
Erik Svedäng <erik@Mac.lan>
2026-03-16 16:45:51 +0100

    Can add layers.

--- demo_neural_network.py
+++ demo_neural_network.py
@@ -1,4 +1,14 @@
 from neural.network import SequentialNetwork
 from neural.layers import DenseLayer, ActivationLayer
+from neural.utils import load_data
 
-n = SequentialNetwork()
+training_data, test_data = load_data()
+
+net = SequentialNetwork()
+
+net.add(DenseLayer(784, 392))
+net.add(ActivationLayer(392))
+net.add(DenseLayer(392, 196))
+net.add(ActivationLayer(196))
+net.add(DenseLayer(196, 10))
+net.add(ActivationLayer(10))
